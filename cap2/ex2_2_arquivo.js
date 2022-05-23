f7991292cbd79b5fff6b13d5d6911f86ab7b9cb4
julianaaelo <julianaaelo3@gmail.com>
2022-05-23 18:04:17 -0300

    Ex2

---
+++ cap2/ex2_2_arquivo.js
@@ -0,0 +1,6 @@
+function converterDuracao() {
+    var nomeDoFilme = document.getElementById("inTitulo").value;
+    var duracao = document.getElementById("inDuracao").value;
+    document.getElementById("outTitulo").textContent = nomeDoFilme;
+    document.getElementById("outDuracao").textContent = Math.floor(duracao/60) + ' Hora(s) e '+ duracao % 60 + ' minuto(s) ';
+};
